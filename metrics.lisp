1b24d4d1980e0a0fec8fdc0be0b1562336d1de41
José Carlos <jcnascimento1994@gmail.com>
2017-10-18 16:28:19 -0300

    Otimizado a função de grau do nó

--- metrics.lisp
+++ metrics.lisp
@@ -1,5 +1,6 @@
 (load "graph.lisp")
-    ;; "Retorna a lista de adjacência do grafo"
+
+;; "Retorna a lista de adjacência do grafo"
 (defmethod adj-list ((g graph))
     (let ((adj-list (make-list (list-length (nodes g)))))
         (dolist (edge (edges g))
@@ -9,24 +10,31 @@
                     (push edge2 (nth (second edge) adj-list)))))
             adj-list))
 
-    ;; "Retorna uma lista com o grau do nó node pertencente ao grafo g, no formato (gOUT, gin).
-    ;; gOUT - Grau de saída
-    ;; gIN - Grau de entrada"
+;; "Retorna uma lista com o grau do nó node pertencente ao grafo g, no formato (gOUT, gin).
+;; gOUT - Grau de saída
+;; gIN - Grau de entrada"
 (defmethod degree ((g graph) node)
-    (let ((degree))
+    (let ((degree) (ad-list))
         (setf degree (list 0 0))
-        (loop for edge-list in (adj-list g) and i from 0 do
-            (if (= node i)
-                (setf (first degree) (list-length edge-list))
-                (let ((count))
-                    (setf count (second degree))
-                    (dolist (edge edge-list)
-                        (if (= (first edge) node)
-                            (setf count (+ 1 count))))
-                    (setf (second degree) count))))
-    degree))
+        (setf ad-list (adj-list g))
+        (setf (first degree) (list-length (nth node ad-list)))
+        (if (= 2 (g-type g))
+            (progn
+                (setf (second degree) (first degree))
+                degree)
+            (progn 
+                (loop for edge-list in (ad-list) and i from 0 do
+                    (if (= node i)
+                        (setf (first degree) (list-length edge-list))
+                        (let ((count))
+                            (setf count (second degree))
+                            (dolist (edge edge-list)
+                                (if (= (first edge) node)
+                                    (setf count (+ 1 count))))
+                            (setf (second degree) count))))
+                degree))))
 
-    ;; "Retorna uma lista com os graus dos nós do grafo g"
+;; "Retorna uma lista com os graus dos nós do grafo g"
 (defmethod degree-list ((g graph))
     (let ((degree-list))
         (setf degree-list (make-list (list-length (nodes g))))
