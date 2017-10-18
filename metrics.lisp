83f3db8f98aab81bbc259f9cd9f9f9969997edfc
José Carlos <jcnascimento1994@gmail.com>
2017-10-18 14:04:32 -0300

    Correção de bugs

--- metrics.lisp
+++ metrics.lisp
@@ -1,26 +1,25 @@
 (load "graph.lisp")
     ;; "Retorna a lista de adjacência do grafo"
-(defmethod get-adj-list ((g graph))
+(defmethod adj-list ((g graph))
     (let ((adj-list (make-list (list-length (nodes g)))))
         (dolist (edge (edges g))
-            (push (rest edge) (nth (- (first edge) 1) adj-list))
+            (push (rest edge) (nth (first edge) adj-list))
             (if (= 2 (g-type g))
-                (let ((edge2 (list (first edge) (third edge))))
-                    (push edge2 (nth (- (second edge) 1) adj-list)))))
+                (let ((edge2 (list (first edge))))
+                    (push edge2 (nth (second edge) adj-list)))))
             adj-list))
 
     ;; "Retorna uma lista com o grau do nó node pertencente ao grafo g, no formato (gOUT, gin).
     ;; gOUT - Grau de saída
     ;; gIN - Grau de entrada"
-(defmethod get-degree ((g graph) node)
-    (let ((degree  (list 0 0)))
-        (loop for edge-list in (get-adj-list g) and i from 1 do
+(defmethod degree ((g graph) node)
+    (let ((degree))
+        (setf degree (list 0 0))
+        (loop for edge-list in (adj-list g) and i from 0 do
             (if (= node i)
-                (let ((count 0))
-                    (dolist (edge edge-list)
-                        (setf count (+ 1 count)))
-                    (setf (first degree) count))
-                (let ((count (second degree)))
+                (setf (first degree) (list-length edge-list))
+                (let ((count))
+                    (setf count (second degree))
                     (dolist (edge edge-list)
                         (if (= (first edge) node)
                             (setf count (+ 1 count))))
@@ -28,15 +27,15 @@
     degree))
 
     ;; "Retorna uma lista com os graus dos nós do grafo g"
-(defmethod get-degree-list ((g graph))
+(defmethod degree-list ((g graph))
     (let ((degree-list))
         (setf degree-list (make-list (list-length (nodes g))))
         (loop for n in (nodes g) and i from 0 do
-            (setf (nth i degree-list) (get-degree g n)))
+            (setf (nth i degree-list) (degree g n)))
         degree-list))
 
     ;; "Retorna a densidade do grafo g"
-(defmethod get-graph-density ((g graph))
+(defmethod graph-density ((g graph))
     (let ((size))
         (setf size (list-length (nodes g)))
         (if (= (g-type g) 1)
