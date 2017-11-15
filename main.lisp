31e1425226c1095b9c6c36c40ee0b6b708bb9d19
José Silva <jcnascimento1994@gmail.com>
2017-11-15 20:17:54 -0300

    Otimizações no código

--- main.lisp
+++ main.lisp
@@ -2,14 +2,14 @@
 (load "tgf-io.lisp")
 (load "metrics.lisp")
 
-;; (defvar graph (load-tgf "ipv6-2008-12-5374-9426.tgf" :g-type 2))
+(defvar graph (load-tgf "ipv6-2008-12-5374-9426.tgf" :g-type 2))
 ;; (defvar graph (load-tgf "tgf.tgf" :g-type 2))
-(defvar graph (random-graph 1000 1 0.001))
+;; (defvar graph (random-graph 300 1 5))
 
 (print-graph-info graph)
 (format t "Desidade do grafo: ~a" (get-density graph))
 (terpri)
 (format t "Grau esperado do grafo: ~a" (get-expt-degree graph t))
 (terpri)
-(format t "(Distância média, Eficiência média) do grafo: ~a" (average-distance graph))
+(format t "Distância média do grafo: ~a" (average-distance graph))
 (terpri)
